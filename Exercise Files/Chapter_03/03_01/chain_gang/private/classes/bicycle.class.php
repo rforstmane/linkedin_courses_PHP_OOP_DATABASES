236f015d403e4eace224b945e51be10cc9221066
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-12-19 09:11:06 +0200

    Found a single record and displayed a detail page

--- Exercise Files/Chapter_03/03_01/chain_gang/private/classes/bicycle.class.php
+++ Exercise Files/Chapter_03/03_01/chain_gang/private/classes/bicycle.class.php
@@ -29,6 +29,17 @@ class Bicycle {
       return self::find_by_sql($sql);
   }
 
+  static public function find_by_id($id) {
+    $sql = "SELECT * FROM bicycles ";
+    $sql .= "WHERE id= '" . self::$database->escape_string($id) . "'";
+    $obj_array= self::find_by_sql($sql);
+    if(!empty($obj_array)) {
+      return array_shift($obj_array);
+    }else {
+      return false;
+    }
+  }
+
   static protected function instantiate($record) {
     $object = new self;
     foreach ($record as $property=>$value) {
@@ -82,6 +93,10 @@ class Bicycle {
     //     $this->$k = $v;
     //   }
     // }
+  }
+
+  public function name() {
+    return "{$this->brand} {$this->model} {$this->year}";
   }
 
   public function weight_kg() {
