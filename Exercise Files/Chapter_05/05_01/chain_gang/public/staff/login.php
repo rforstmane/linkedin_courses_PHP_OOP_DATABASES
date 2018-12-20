0885bd9932752e37239e5ba869e441f53934c900
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-12-20 15:27:28 +0200

    Verified password

--- Exercise Files/Chapter_05/05_01/chain_gang/public/staff/login.php
+++ Exercise Files/Chapter_05/05_01/chain_gang/public/staff/login.php
@@ -22,7 +22,7 @@ if(is_post_request()) {
   if(empty($errors)) {
     $admin = Admin::find_by_username($username);
     // test if admin found and password is correct
-    if($admin) {
+    if($admin != false && $admin->verify_password($password)) {
       // Mark admin as logged in
       redirect_to(url_for('/staff/index.php'));
     } else {
